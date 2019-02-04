2f2fcb220dae06951701976db3ccb83e9831d513
yolanda <ysimpson@spartaglobal.com>
2019-02-04 16:02:22 +0000

    grid added to medium page

--- js/index.js
+++ js/index.js
@@ -1,27 +1,21 @@
 console.log("works");
-//var makeGrid =
-// [
-// [0,0,0,0,0,0,0],
-// [0,0,0,0,0,0,0],
-// [0,0,0,0,0,0,0],
-// [0,0,0,0,0,0,0],
-// [0,0,0,0,0,0,0],
-// [0,0,0,0,0,0,0]];
+var gridArray =
+[
+ [0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0],
+ [0,0,0,0,0,0,0]
+];
 
 
-
-// function drawTiles() {
-//   for (var i = 0; i < makeGrid.length; i++) {
-//     var tile = document.createElement('div');
-//     tile.classList.add("tile")
-//     gamebody.appendChild.innerHTML()= getElementsByClassName('tile');
-//   }
-//
-// }
 function makeGrid() {
-    for (var rows = 0; rows <=5; rows++) {
-        for (var columns = 0; columns < 6; columns++) {
-            $("#gamebody").append("<div class='grid'></div>");
+    for (var y = 0; y <=5; y++) {
+    for (var  x= 0; x <=6; x++) {
+            $("#gamebody").append("<div class='tiles'></div>");
         }
     }
 };
+
+makeGrid();
