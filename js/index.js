778a9f11789d78e1981e479b7ea297b19c1cfa94
yolanda <ysimpson@spartaglobal.com>
2019-02-05 12:26:29 +0000

    manually changed a square colour

--- js/index.js
+++ js/index.js
@@ -1,21 +1,53 @@
-console.log("works");
-var gridArray =
-[
- [0,0,0,0,0,0,0],
- [0,0,0,0,0,0,0],
- [0,0,0,0,0,0,0],
- [0,0,0,0,0,0,0],
- [0,0,0,0,0,0,0],
- [0,0,0,0,0,0,0]
-];
+var level = 0;
+
+// var gridArray =
+// [
+//  [0,0,0,0,0],
+//  [0,0,0,0,0],
+//  [0,0,0,0,0],
+//  [0,0,0,0,0],
+//  [0,0,0,0,0]
+// ];
 
 
-function makeGrid() {
-    for (var y = 0; y <=5; y++) {
-    for (var  x= 0; x <=6; x++) {
-            $("#gamebody").append("<div class='tiles'></div>");
-        }
+// var blockColour = document.getElementById('game-block1');
+// for (var i = 0; i < blockColour.length; i++) {
+//   console.log("works");
+//
+//   }
+
+var blockColour = document.getElementById("game-squares");
+
+
+blockColour.addEventListener("click", squares);
+//blockColour.addEventListener("click", someOtherFunction);
+
+function squares (event){
+
+      $("#game-block1").addClass("squareColour");
     }
-};
 
-makeGrid();
+// function makeGrid() {
+//     for (var y = 0; y <5; y++) {
+//     for (var  x= 0; x <5; x++) {
+//             $("#gamebody").append("<div class='tiles'></div>");
+//         }
+//     }
+// };
+//
+// makeGrid();
+//
+// var blockColour = Math.random();
+//     if (blockColour = 1)
+//   }
+
+
+
+// var compChoice = Math.random();
+//   if (compChoice < 0.35) {
+// 	alert(compChoice = "the computer chose rock");
+// } else if(compChoice <= 0.68) {
+// 	alert(compChoice = "the computer chose paper");
+//   } else {
+// 	alert(compChoice = "the computer chose scissors");
+//   }
