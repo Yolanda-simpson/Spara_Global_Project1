87ca38f409c6cd567ce080652e28ce4705142417
yolanda <ysimpson@spartaglobal.com>
2019-02-06 14:52:42 +0000

    new additions

--- js/index.js
+++ js/index.js
@@ -1,26 +1,21 @@
 var level = 0;
 var score = 0;
-
-// var gridArray =
-// [
-//  [0,0,0,0,0],
-//  [0,0,0,0,0],
-//  [0,0,0,0,0],
-//  [0,0,0,0,0],
-//  [0,0,0,0,0]
-// ];
+var userChoice = [];
 
 
-// var blockColour = document.getElementById('game-block1');
+
+
+
+// var blockColour = document.getElementById('game-squares');
 // for (var i = 0; i < blockColour.length; i++) {
 //   console.log("works");
 //
 //   }
-
-var blockColour = document.getElementById("game-squares");
-
-
-blockColour.addEventListener("click", squares);
+//
+// var blockColour = document.getElementById("game-squares");
+//
+//
+// blockColour.addEventListener("click", squares);
 //blockColour.addEventListener("click", someOtherFunction);
 
 
@@ -32,11 +27,15 @@ function squares (event){
     }
 
 
-    var r1 = Math.floor(Math.random() * 36);
-    var r2 = Math.floor(Math.random() * 36);
-    var r3 = Math.floor(Math.random() * 36);
+    var r1 = Math.floor(Math.random() * 36)+1;
+    var r2 = Math.floor(Math.random() * 36)+1;
+    var r3 = Math.floor(Math.random() * 36)+1;
 
-    var storeRandomSquare = (this);
+    // for (var i = 0; i < array.length; i++) {
+    //   array[i]
+    // }
+
+    // var storeRandomSquare = (this);
 
     // function clearSquare() {
     //   t =setTimeout(color, 3000);
@@ -44,20 +43,92 @@ function squares (event){
     // }
 
     setTimeout(function(){
-      $("#"+r1).css("background-color", "#FFF");
-      $("#"+r2).css("background-color", "#FFF");
-      $("#"+r3).css("background-color", "#FFF");
+      $("#"+r1).addClass("clearBlock");
+      $("#"+r2).addClass("clearBlock");
+      $("#"+r3).addClass("clearBlock");
       // $('#'+r1).remove();
       // $('#'+r2).remove();
       // $('#'+r3).remove();
     }, 4000);
 
-//if statment to alternate between colours black and white
+//clicking square blocks
+$(function () {
+    $("#"+r1).click(function () {
+        $(this).css('background-color', '#000000');
+    });
+});
+$(function () {
+    $("#"+r2).click(function () {
+        $(this).css('background-color', '#000000');
+    });
+});
+$(function () {
+    $("#"+r3).click(function () {
+        $(this).css('background-color', '#000000');
+    });
+});
+
+
+
+// var clickBox = document.querySelectorAll('.squareColour')
+// for (var i = 0; i < clickBox.length; i++) {
+//   clickBox[i].addEventListener('click',function (event) {
+//     console.log(this.value + 'was clicked ');
+// })
+// };
+// var clickBlock = document.getElementById('r1');
+//
+//
+// clickBlock.onclick = function() {
+//   this.style.backgroundColor = 'black';
+// };
+// clickBlock.onmouseover = function() {
+//     this.style.backgroundColor = 'red';
+//   }
+
+ // document.getElementById(r1).addEventListener("click", function(event) {
+ //   // var clickBlock = document.getElementById(r1);
+ //   //  clickBlock.classList.add("squareColour");
+ //     console.log(event.target);
+ //     $('game-block').click(function(){
+ //       $(r1).addClass("squareColour")
+ //     })
+ // });
+
+ // function userClicks (){
+ //
+ //   document.getElementById('1').addClass("squareColour");
+ //   console.log('works');
+ // var blockColour = document.getElementById("game-squares");
+ //
+ //
+ // blockColour.addEventListener("click", squares);
+ //   document.addEventListener("click", function(){
+ //     document.getElementById("#").appendChild(".squareColour");
+ //   });
+
+
+// var blockColour = $("#"+r1).addClass("squareColour");
+// blockColour.addEventListener("click", squares);
+// for (var i = 0; i < game-squares.length; i++) {
+//    console.log("works");
+//
+// }
+// }
 
 
 
 
 
+// $(".squareColour").mousedown(function(){
+//         if (playersTurn) {
+//             var gameBlockID = '#'+r1 + this.id;
+//             var highlightColor = $(this).css('squareColour');
+// $("1").addClass("squareColour");
+//
+// }
+//
+// });
 
 
 
